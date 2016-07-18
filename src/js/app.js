e11b51297ed11ce2bfdc1d55a1fb51417946d848
Dan Kirkham <Daniel.A.Kirkham@gmail.com>
2016-07-18 16:48:25 -0500

    Added license

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,18 @@
+// This file is part of PogoCalc.
+//
+// PogoCalc is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// PogoCalc is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with PogoCalc.  If not, see <http://www.gnu.org/licenses/>.
+
 'use strict';
 
 var app = angular.module('PogoCalc', [])
@@ -112,7 +127,7 @@ function PogoCalcController($scope) {
         resources.pidgeys = resources.pidgeys - pidgeyToTransfer;
         resources.candy = resources.candy + pidgeyToTransfer;
         evolve(calculations, resources);
-        
+
       } else {
         done = true;
       }
